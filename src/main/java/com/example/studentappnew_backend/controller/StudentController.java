d73af6d00fb586b4bb66db598c324603dadd1ed5
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-08 15:01:13 +0530

    search

--- src/main/java/com/example/studentappnew_backend/controller/StudentController.java
+++ src/main/java/com/example/studentappnew_backend/controller/StudentController.java
@@ -5,7 +5,9 @@ import com.example.studentappnew_backend.model.Students;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StudentController {
@@ -18,19 +20,29 @@ public class StudentController {
     }
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
-    public String AddStudent(@RequestBody Students s)
+    public Map<String,String> AddStudent(@RequestBody Students s)
    {
        System.out.println(s.getName().toString());
        System.out.println(s.getAdmno().toString());
        System.out.println(s.getRollno());
        System.out.println(s.getCollege().toString());
        dao.save(s);
-       return "student added successfully";
+       HashMap<String,String> map=new HashMap<>();
+       map.put("status","success");
+       return map;
    }
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
     public List<Students> ViewStudent()
    {
        return (List<Students>) dao.findAll();
+   }
+
+   @PostMapping(path = "/search",consumes = "application/json",produces ="application/json")
+    public  List<Students> SearchStudent(@RequestBody Students s)
+   {
+       String admno=s.getAdmno().toString();
+       System.out.println(admno);
+       return (List<Students>) dao.SearchStudent(s.getAdmno());
    }
 }
