e484ae4150a1d34f5d72c58a74d42c1c67e35745
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-08 14:37:20 +0530

    add and view

--- src/main/java/com/example/studentappnew_backend/controller/StudentController.java
+++ src/main/java/com/example/studentappnew_backend/controller/StudentController.java
@@ -1,23 +1,36 @@
 package com.example.studentappnew_backend.controller;
 
+import com.example.studentappnew_backend.dao.StudentDao;
 import com.example.studentappnew_backend.model.Students;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
+    @Autowired
+    private StudentDao dao;
     @GetMapping("/")
     public String HomePage()
     {
         return "Welcome to my home page";
     }
-
-   @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public String AddStudent(@RequestBody Students s)
    {
        System.out.println(s.getName().toString());
+       System.out.println(s.getAdmno().toString());
+       System.out.println(s.getRollno());
+       System.out.println(s.getCollege().toString());
+       dao.save(s);
        return "student added successfully";
+   }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/view")
+    public List<Students> ViewStudent()
+   {
+       return (List<Students>) dao.findAll();
    }
 }
